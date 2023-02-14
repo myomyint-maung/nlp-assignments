79239d2b1783c07c6b9f9e7d91d2b601083544a5
Myo Myint Maung <95136195+myomyint-maung@users.noreply.github.com>
2023-02-14 15:07:38 +0700

    update

--- 04-Resume-Parser/main.py
+++ 04-Resume-Parser/main.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, render_template, flash, redirect
 from werkzeug.utils import secure_filename
 import os
-from parser import get_skills_education
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS
+from PyPDF2 import PdfReader
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretkey'
@@ -16,6 +18,73 @@ def remove_uploaded_resume():
      file_list = os.listdir('static/files')
      for filename in file_list:
           os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+def get_skills_education(file_path):
+
+    # Load NLP model
+    nlp = spacy.load('en_core_web_md')
+
+    # Add entity ruler to NLP
+    ruler = nlp.add_pipe("entity_ruler", before="ner")
+
+    # Load skill and education lables to the ruler
+    ruler.from_disk('static/data/skills_and_education.jsonl')
+
+    # Load the PDF file
+    reader = PdfReader(file_path)
+
+    # Extract text from the PDF
+    text = str()
+
+    for i in range(len(reader.pages)):
+        page = reader.pages[i]
+    
+        if i == 0:
+            text += page.extract_text()
+        else:
+            text += ' ' + page.extract_text()
+    
+    # Preprocess the text    
+    stopwords = list(STOP_WORDS)
+    doc = nlp(text)
+    cleaned_tokens = []
+    
+    for token in doc:
+        if token.text not in stopwords and token.pos_ != 'PUNCT' and \
+            token.pos_ != 'SPACE' and token.pos_ != 'SYM':
+                cleaned_tokens.append(token.text.lower().strip())
+    
+    text = " ".join(cleaned_tokens)
+
+    # Extract sklls and education
+    doc = nlp(text)
+    skills = []
+    education = []
+
+    for i in range(len(doc.ents)):
+        
+        if doc.ents[i].label_ == "SKILL":
+            skills.append(doc.ents[i].text)
+        
+        if doc.ents[i].label_ == "EDUCATION_PRO":
+            education.append(doc.ents[i].text)
+        
+        if doc.ents[i].label_ == "EDUCATION_OF" and doc.ents[i+1].label_ == "SKILL" and doc.ents[i+2].label_ == "SKILL":
+            education.append(doc.ents[i].text + ' of ' + doc.ents[i+1].text + ' in ' + doc.ents[i+2].text)
+        elif doc.ents[i].label_ == "EDUCATION_OF" and doc.ents[i+1].label_ == "SKILL" and doc.ents[i+2].label_ != "SKILL":
+            education.append(doc.ents[i].text + ' of ' + doc.ents[i+1].text)
+        
+        if doc.ents[i].label_ == "EDUCATION_IN" and doc.ents[i+1].label_ == "SKILL":
+            education.append(doc.ents[i].text + ' in ' + doc.ents[i+1].text)
+
+    skills = list(set(skills))
+    education = list(set(education))
+
+    skills.sort()
+    education.sort()
+    education.reverse()
+
+    return skills, education
 
 @app.route('/')
 @app.route('/home')
