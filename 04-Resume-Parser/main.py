49805e8bd7a5cbb3a90ee291ddd169e725e36030
Myo Myint Maung <95136195+myomyint-maung@users.noreply.github.com>
2023-02-14 11:11:14 +0700

    update

--- 04-Resume-Parser/main.py
+++ 04-Resume-Parser/main.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import FileField
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretkey'
 
-@app.route('/')
-@app.route('/home')
+class UploadFile(FlaskForm):
+    file = FileField("file")
+    submit = SubmitField("Upload File")
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    form = UploadFile()
+    return render_template('home.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
