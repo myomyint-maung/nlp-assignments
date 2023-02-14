43a0505dc5b270ccc4a8f16fac05e1ed12259aa3
Myo Myint Maung <95136195+myomyint-maung@users.noreply.github.com>
2023-02-14 10:54:41 +0700

    upload

--- 04-Resume-Parser/main.py
+++ 04-Resume-Parser/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return "<b><i>Resume Parser<i></b> by Myo Myint Maung"
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug==True)
