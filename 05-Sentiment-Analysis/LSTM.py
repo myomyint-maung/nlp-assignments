cdf6517ec29a24bc8695e3e46b87fbc2fa90e444
Myo Myint Maung <95136195+myomyint-maung@users.noreply.github.com>
2023-02-16 14:25:59 +0700

    update

---
+++ 05-Sentiment-Analysis/LSTM.py
@@ -0,0 +1,7 @@
+# Import necessary libraries
+import torch, torchtext
+import torch.nn as nn
+import pytreebank
+from torchtext.data.utils import get_tokenizer
+from torchtext.vocab import build_vocab_from_iterator
+from torchtext.vocab import FastText
