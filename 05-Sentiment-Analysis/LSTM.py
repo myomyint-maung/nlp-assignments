708af4b1167c57454efa4a807f4b7a929c9f1736
Myo Myint Maung <95136195+myomyint-maung@users.noreply.github.com>
2023-02-16 14:28:16 +0700

    update

--- 05-Sentiment-Analysis/LSTM.py
+++ 05-Sentiment-Analysis/LSTM.py
@@ -5,3 +5,7 @@ import pytreebank
 from torchtext.data.utils import get_tokenizer
 from torchtext.vocab import build_vocab_from_iterator
 from torchtext.vocab import FastText
+
+# Choose the computing device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
