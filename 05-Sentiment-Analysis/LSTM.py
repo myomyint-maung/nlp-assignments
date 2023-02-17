bc2e8d49712a547892609f4f32aee1480a3d59d4
Myo Myint Maung <95136195+myomyint-maung@users.noreply.github.com>
2023-02-17 11:18:05 +0700

    update

--- 05-Sentiment-Analysis/LSTM.py
+++ 05-Sentiment-Analysis/LSTM.py
@@ -34,10 +34,6 @@ vocab.set_default_index(vocab['<unk>'])
 # Set the padding index
 pad_idx = vocab['<pad>']
 
-# Load FastText embeddings for the vocab
-fast_vectors = FastText(language='simple')
-fast_embedding = fast_vectors.get_vecs_by_tokens(vocab.get_itos()).to(device)
-
 # The LSTM model
 class LSTM(nn.Module):
     def __init__(self, input_dim, emb_dim, hid_dim, output_dim, num_layers, bidirectional, dropout):
@@ -63,3 +59,30 @@ class LSTM(nn.Module):
         hn = torch.cat((hn[-2,:,:], hn[-1,:,:]), dim = 1)
         
         return self.fc(hn)
+
+# The model's parameters
+input_dim  = len(vocab)
+hid_dim    = 256
+emb_dim    = 300
+output_dim = 5
+num_layers = 2
+bidirectional = True
+dropout = 0.5
+
+model = LSTM(input_dim, emb_dim, hid_dim, output_dim, num_layers, bidirectional, dropout).to(device)
+
+# Load the train model
+save_path = f'models/LSTM.pt'
+model.load_state_dict(torch.load(save_path))
+
+# Prediction
+def prediction(text_list):
+    result = list()
+    for text in text_list:
+        text = torch.tensor(text_pipeline(text)).to(device)
+        text = text.reshape(1, -1)
+        text_length = torch.tensor([text.size(1)]).to(dtype=torch.int64)
+        output = model(text, text_length).squeeze(1)
+        predicted = torch.max(output.data, 1)[1].detach().cpu().numpy()[0]
+        result.append((test_str, predicted))
+    return result
