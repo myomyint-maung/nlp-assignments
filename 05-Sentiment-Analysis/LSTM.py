ee601521c19424c85840f880abb8f956ecd96909
Myo Myint Maung <95136195+myomyint-maung@users.noreply.github.com>
2023-02-19 14:17:04 +0700

    update

--- 05-Sentiment-Analysis/LSTM.py
+++ 05-Sentiment-Analysis/LSTM.py
@@ -5,22 +5,42 @@ import torch.nn as nn
 from torchtext.data.utils import get_tokenizer
 from torchtext.vocab import build_vocab_from_iterator
 from torchtext.vocab import FastText
+from torch.utils.data   import DataLoader
+from torch.nn.utils.rnn import pad_sequence
 
 # Choose the computing device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Load the training data
+# Set SEED for reproducibility
+SEED = 1234
+torch.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+# Load the Stanford Sentiment Treebank dataset
 sst = pytreebank.load_sst()
+
+# Extract the training, validation and test sets
 train  = sst['train']
+val    = sst['dev']
+test   = sst['test']
 
+# Extract the training, validation and test data
+datasets    = [train, val, test]
 train_data  = []
-for example in train:
-    for sentiment, text in example.to_labeled_lines():
-            train_data.append((sentiment, text))
+val_data    = []
+test_data   = []
+data        = [train_data, val_data, test_data]
 
-# Create the vocab
+for i in range(len(data)):
+  dataset = datasets[i]
+  for example in dataset:
+    for sentiment, text in example.to_labeled_lines():
+      data[i].append((sentiment, text))
+
+# Create a tokenizer
 tokenizer = get_tokenizer('spacy', language='en_core_web_sm')
 
+# Create the vocab
 def yield_tokens(data_iter):
     for data in data_iter:
         for _, text in data.to_labeled_lines():
@@ -33,6 +53,44 @@ vocab.set_default_index(vocab['<unk>'])
 
 # Set the padding index
 pad_idx = vocab['<pad>']
+
+# Load FastText embeddings
+fast_vectors = FastText(language='simple')
+fast_embedding = fast_vectors.get_vecs_by_tokens(vocab.get_itos()).to(device)
+
+# Create text and label pipelines
+text_pipeline  = lambda x: vocab(tokenizer(x))
+label_pipeline = lambda x: int(x) - 1
+
+# Create a function to collate batches
+def collate_batch(batch):
+    label_list, text_list, length_list = [], [], []
+    for (_label, _text) in batch:
+        label_list.append(_label)
+        processed_text = torch.tensor(text_pipeline(_text), dtype=torch.int64)
+        text_list.append(processed_text)
+        length_list.append(processed_text.size(0))
+        
+    return torch.tensor(label_list, dtype=torch.int64), \
+        pad_sequence(text_list, padding_value=pad_idx, batch_first=True), \
+        torch.tensor(length_list, dtype=torch.int64)
+
+# Prepare dataloaders
+batch_size = 64
+
+train_loader = DataLoader(train_data, batch_size = batch_size,
+                          shuffle=True, collate_fn=collate_batch)
+
+val_loader   = DataLoader(val_data, batch_size = batch_size,
+                          shuffle=True, collate_fn=collate_batch)
+
+test_loader  = DataLoader(test_data, batch_size = batch_size,
+                          shuffle=True, collate_fn=collate_batch)
+
+# Define the lengths of the data loaders
+train_loader_length = len(list(iter(train_loader)))
+val_loader_length   = len(list(iter(val_loader)))
+test_loader_length  = len(list(iter(test_loader)))
 
 # The LSTM model
 class LSTM(nn.Module):
