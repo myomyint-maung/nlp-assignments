a382f66a94b670fa704db4a434808ecb985f2065
Myo Myint Maung <95136195+myomyint-maung@users.noreply.github.com>
2023-02-20 05:30:31 +0700

    update

--- 05-Sentiment-Analysis/LSTM_model.py
+++ 05-Sentiment-Analysis/LSTM_model.py
@@ -1,12 +1,69 @@
 # Import necessary libraries
-import torch, torchtext, torchdata
+import torch
 from torch import nn
+from torchtext.datasets import SST2
+from torchtext.data.utils import get_tokenizer
+from torchtext.vocab import build_vocab_from_iterator
 
-# Choose computing device
+# Choose the computing device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Set SEED for reproducibility
-SEED = 1234
-torch.manual_seed(SEED)
-torch.backends.cudnn.deterministic = True
+# Load training data
+train = SST2(split='train')
+
+# Create vocab
+tokenizer = get_tokenizer('spacy', language='en_core_web_sm')
+
+def yield_tokens(data_iter):
+    for text, _ in data_iter:
+        yield tokenizer(text)
+        
+vocab = build_vocab_from_iterator(yield_tokens(train),
+                                  specials=['<unk>','<pad>','<bos>','<eos>'])
+
+# Set <unk> as the default index of the vocab
+vocab.set_default_index(vocab['<unk>'])
+
+# Set the padding index
+pad_idx = vocab['<pad>']
+
+# Create the LSTM model
+class LSTM(nn.Module):
+    def __init__(self, input_dim, emb_dim, hid_dim, output_dim, num_layers, bidirectional, dropout):
+        super().__init__()
+        #put padding_idx so asking the embedding layer to ignore padding
+        self.embedding = nn.Embedding(input_dim, emb_dim, padding_idx=pad_idx)
+        self.lstm = nn.LSTM(emb_dim, 
+                           hid_dim, 
+                           num_layers=num_layers, 
+                           bidirectional=bidirectional, 
+                           dropout=dropout,
+                           batch_first=True)
+        self.fc = nn.Linear(hid_dim * 2, output_dim)
+        
+    def forward(self, text, text_lengths):
+        #text = [batch size, seq len]
+        embedded = self.embedding(text)
+        
+        #++ pack sequence ++
+        packed_embedded = nn.utils.rnn.pack_padded_sequence(embedded, text_lengths.to('cpu'), enforce_sorted=False, batch_first=True)
+        
+        #embedded = [batch size, seq len, embed dim]
+        packed_output, (hn, cn) = self.lstm(packed_embedded)  #if no h0, all zeroes
+        
+        #++ unpack in case we need to use it ++
+        output, output_lengths = nn.utils.rnn.pad_packed_sequence(packed_output, batch_first=True)
+        
+        #output = [batch size, seq len, hidden dim * num directions]
+        #output over padding tokens are zero tensors
+        
+        #hidden = [num layers * num directions, batch size, hid dim]
+        #cell = [num layers * num directions, batch size, hid dim]
+        
+        #concat the final forward (hidden[-2,:,:]) and backward (hidden[-1,:,:]) hidden layers
+        hn = torch.cat((hn[-2,:,:], hn[-1,:,:]), dim = 1)
+        #hn = [batch size, hidden dim * num directions]
+        
+        return self.fc(hn)
+
 
