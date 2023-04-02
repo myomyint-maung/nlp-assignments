1ddb6486fc23e5d298c7649410e694654e2b1372
Myo Myint Maung <95136195+myomyint-maung@users.noreply.github.com>
2023-04-03 06:54:23 +0700

    update

--- 07-Machine-Translation/translator_app.py
+++ 07-Machine-Translation/translator_app.py
@@ -8,21 +8,21 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretkey'
 
 class MyForm(FlaskForm):
-    name = StringField('Input Myanmar', validators=[DataRequired()])
+    source = StringField('Input Myanmar', validators=[DataRequired()])
     submit = SubmitField('Translate')
 
 @app.route('/')
 @app.route('/translator', methods = ['GET','POST'])
 def translator():
     form = MyForm()
-    name = False
-    code = False
+    source = False
+    target = False
     print(form.validate_on_submit())
     if form.validate_on_submit():
-        name = form.name.data 
-        code = translate(name)
-        form.name.data = ""
-    return render_template("translator.html", form=form, name=name, code=code)
+        source = form.source.data 
+        target = translate(source)
+        form.source.data = ""
+    return render_template("translator.html", form=form, source=source, target=target)
 
 if __name__ == '__main__':
     app.run(debug=True)
