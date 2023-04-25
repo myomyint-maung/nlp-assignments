27a9e462f7bbd900a81c7b140161503f48201c2d
Myo Myint Maung <95136195+myomyint-maung@users.noreply.github.com>
2023-04-25 21:22:37 +0700

    update

--- 08-Transformer-Language-Model/autocomplete_app.py
+++ 08-Transformer-Language-Model/autocomplete_app.py
@@ -8,21 +8,21 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretkey'
 
 class MyForm(FlaskForm):
-    name = StringField('Input code', validators=[DataRequired()])
+    prompt = StringField('Input python code', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 @app.route('/')
 @app.route('/autocomplete', methods = ['GET','POST'])
 def autocomplete():
     form = MyForm()
-    name = False
+    prompt = False
     code = False
     print(form.validate_on_submit())
     if form.validate_on_submit():
-        name = form.name.data 
-        code = predict(name)
-        form.name.data = ""
-    return render_template("autocomplete.html", form=form, name=name, code=code)
+        prompt = form.prompt.data 
+        code = predict(prompt)
+        form.source.data = ""
+    return render_template('autocomplete.html', form=form, prompt=prompt, code=code)
 
 if __name__ == '__main__':
     app.run(debug=True)
